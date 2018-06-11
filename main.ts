42fb81b3c663243fcb2397646d99fcf65d48f155
uemura <uemura@Yusuke-Mac.local>
2018-06-11 15:30:44 +0900

    init-commit

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .)
+“main.ts”
+//% weight=70 icon="\u30A1" color=#EC7505 block="test"
+namespace test {
+
+    //% blockId=show_strings block="hogehoge %v"
+    export function putStr(text: string): void {
+      basic.showLeds(`
+          . . . . .
+          . # . # .
+          . . . . .
+          # . . . #
+          . # # # .
+          `);
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
+// tests go here; this will not be compiled when this package is used as a librar
+“test.ts”
 // tests go here; this will not be compiled when this package is used as a library
+basic.forever(() => {
+  test.putStr("カタカナ")
+})y
